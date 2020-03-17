dcff48ff3f2f4cfe102051935ed95edf7d795742
Th3Un1q3 <19877114+Th3Un1q3@users.noreply.github.com>
2020-03-17 11:54:43 +0200

    fixed typo

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>
       Where to start from?
     </h1>
-    <span>Root vue is views/PasswordCheck.vue</span>
+    <span>Root view is views/PasswordCheck.vue</span>
     <h1>Is there more detailed requirements?</h1>
     <span>Follow the test in 'tests/unit/PasswordCheck.spec.js'</span>
     <router-view/>
