51d4e090f6576908c0ed167fb03cd5e118acd4be
ivan <ivansolianyk@admins-MacBook-Pro-3.local>
2020-07-27 22:42:51 +0300

    feat(defineAsyncComponent): async component loading

--- src/views/PasswordCheck.vue
+++ src/views/PasswordCheck.vue
@@ -2,34 +2,44 @@
   <div>
     <PasswordInput :value="pass" @updated-value="updatedValue" />
     <Rules :valid-rules="validRules"/>
-    <PasswordStrengthIndicator
+    <AsyncPasswordStrengthIndicator
       data-test="password-strength"
       :status="weakOrStrong"
     />
   </div>
 </template>
 <script>
-import PasswordInput from '../components/PasswordInput.vue';
-import PasswordStrengthIndicator from '../components/PasswordStrengthIndicator.vue';
-import Rules from '../components/Rules.vue';
+import { defineAsyncComponent } from 'vue';
 import usePasswordValidation from '@/composition/usePasswordValidation';
+import PasswordInput from '../components/PasswordInput.vue';
+import Rules from '../components/Rules.vue';
+
+const AsyncPasswordStrengthIndicator = defineAsyncComponent({
+  loader: () => import('../components/PasswordStrengthIndicator.vue' /* webpackChunkName: 'strength' */),
+  loadingComponent: PasswordInput,
+  delay: 2000,
+  suspensible: false,
+});
 
 export default {
+  name: 'PasswordCheck',
   components: {
     Rules,
     PasswordInput,
-    PasswordStrengthIndicator,
+    AsyncPasswordStrengthIndicator,
   },
   setup() {
-    const { password: pass, updatedValue, validRules, weakOrStrong } = usePasswordValidation();
+    const {
+      password: pass, updatedValue, validRules, weakOrStrong
+    } = usePasswordValidation();
 
     return {
       pass,
       updatedValue,
       validRules,
-      weakOrStrong
-    }
-  }
+      weakOrStrong,
+    };
+  },
 };
 </script>
 <style>
